e2e8fc4a3d99c4dcd93568b89fac8e66db67bc28
imfaazi <faazfajib7@gmail.com>
2019-06-21 15:30:26 +0530

    add level 1

---
+++ PuzzleGame/Assets/Project Assets/Scripts/Controller.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    //public List<GameObject> spritesBG;
+    //public List<GameObject> sprites;
+
+    private Vector2 _initialPos;
+    public Transform targetPos;
+    private float _deltaX, _deltaY;
+
+    void Start()
+    {
+        _initialPos = transform.position;
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPosition))
+                    {
+                        _deltaX = touchPosition.x - transform.position.x;
+                        _deltaY = touchPosition.y - transform.position.y;
+                    }
+                    break;
+
+                case TouchPhase.Moved:
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPosition))
+                    {
+                        transform.position = new Vector2(touchPosition.x - _deltaX, touchPosition.y - _deltaY);
+                    }
+                    break;
+
+                case TouchPhase.Ended:
+                    if (Mathf.Abs(transform.position.x - targetPos.position.x) <= 0.5f &&
+                    Mathf.Abs(transform.position.y - targetPos.position.y) <= 0.5f)
+                    {
+                        transform.position = new Vector2(targetPos.position.x, targetPos.position.y);
+                    }
+
+                    else
+                    {
+                        transform.position = new Vector2(_initialPos.x, _initialPos.y);
+                    }
+                    break;
+            }
+
+        }
+    }
+}
+
